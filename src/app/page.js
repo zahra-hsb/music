3e42e73107f70e55b1ec4df4204e792409293609
zahra <zahra20hb@gmail.com>
2024-10-12 12:56:01 -0700

    musics categpry section created

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import TextContainer from "@/components/globalComponents/TextContainer";
 import CategoriesSection from "@/components/homeComponents/CategoriesSection";
 import FamousMusicsSection from "@/components/homeComponents/FamousMusicsSection";
 import MainSlider from "@/components/homeComponents/MainSlider";
+import TextCatSection from "@/components/homeComponents/TextCatSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <CategoriesSection />
       <TextContainer text={'این اپ برای لحظاتی که می‌خوای با دوستانت همصدایی کنی، ساخته شده.'} />
       <FamousMusicsSection />
+      <TextCatSection />
     </>
   );
 }
---
+++ src/components/homeComponents/TextCatSection/index.jsx
@@ -0,0 +1,45 @@
+'use client'
+import { Swiper, SwiperSlide } from "swiper/react"
+import React, { createContext } from 'react';
+// Import Swiper styles
+import 'swiper/css';
+
+const catsArray = [
+    {id: 1, title: 'پاپ'},
+    {id: 2, title: 'سنتی'},
+    {id: 3, title: 'جدید'},
+    {id: 4, title: 'راک'},
+    {id: 5, title: 'قدیمی'},
+]
+
+const TextCatSection = () => {
+    return (
+        <>
+            <section className="p-5">
+                <h4>دسته بندی آهنگ ها</h4>
+                <Swiper
+                    slidesPerView={4.5}
+                    spaceBetween={15}
+                    centeredSlides={false}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    loop={true}
+                    className="mySwiper"
+                >
+                    {catsArray?.map(item => (
+                        <>
+                            <SwiperSlide>
+                                <div className="text-center px-3 py-2 bg-gray-300 w-full rounded-2xl">
+                                    {item.title}
+                                </div>
+                            </SwiperSlide>
+                        </>
+                    ))}
+                </Swiper>
+            </section>
+        </>
+    )
+}
+
+export default TextCatSection
