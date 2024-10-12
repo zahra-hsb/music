d99ae2e65777a6570a5199909e97963e27b2f4c6
zahra <zahra20hb@gmail.com>
2024-10-12 11:26:37 -0700

    categories slider created

--- src/components/globalComponents/CatSlider/index.jsx
+++ src/components/globalComponents/CatSlider/index.jsx
@@ -1,33 +1,46 @@
 import { Swiper, SwiperSlide } from "swiper/react"
-import React, { createContext } from 'react'; 
+import React, { createContext } from 'react';
 // Import Swiper styles
 import 'swiper/css';
+
+import pic1 from '../../../../public/pictures/Ellipse 4.png'
+import pic2 from '../../../../public/pictures/Ellipse 5.png'
+import pic3 from '../../../../public/pictures/Ellipse 7.png'
+import pic4 from '../../../../public/pictures/Ellipse 8.png'
+import Image from "next/image";
+
+const picsArray = [
+    { id: 1, pic: pic1 },
+    { id: 2, pic: pic2 },
+    { id: 3, pic: pic3 },
+    { id: 4, pic: pic4 },
+    { id: 5, pic: pic3 },
+    { id: 6, pic: pic2 },
+]
 
 const CatSlider = () => {
     return (
         <>
-<div>
-                    <Swiper
-                        slidesPerView={4}
-                        spaceBetween={30}
-                        centeredSlides={true}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        // modules={[]}
-                        className="mySwiper"
-                    >
-                        <SwiperSlide>Slide 1</SwiperSlide>
-                        <SwiperSlide>Slide 2</SwiperSlide>
-                        <SwiperSlide>Slide 3</SwiperSlide>
-                        <SwiperSlide>Slide 4</SwiperSlide>
-                        <SwiperSlide>Slide 5</SwiperSlide>
-                        <SwiperSlide>Slide 6</SwiperSlide>
-                        <SwiperSlide>Slide 7</SwiperSlide>
-                        <SwiperSlide>Slide 8</SwiperSlide>
-                        <SwiperSlide>Slide 9</SwiperSlide>
-                    </Swiper>
-                </div>
+            <div className="w-full py-5">
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    centeredSlides={false}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    // modules={[]}
+                    className="mySwiper"
+                >
+                    {picsArray?.map(item => (
+                        <>
+                            <SwiperSlide>
+                                <Image className="w-full" src={item.pic} key={item.id} alt="" />
+                            </SwiperSlide>
+                        </>
+                    ))}
+                </Swiper>
+            </div>
         </>
     )
 }
